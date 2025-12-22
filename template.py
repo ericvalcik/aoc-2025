5ec584067278f7bed2786550e8d5ce2bf062c43e
eric valcik <eric.valcik@gmail.com>
2025-12-22 21:51:28 +0100

    fix template

--- template.py
+++ template.py
@@ -1,58 +1,16 @@
-"""
-Advent of Code 2025 - Day XX Template
-
-Copy this file to dayXX/solution.py and fill in your solution.
-"""
-
-import sys
-from pathlib import Path
-
-# Add parent directory to path to import utils
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
-from utils import read_input, read_input_raw
+from utils import read_input
 
 
 def part1(data):
-    """
-    Solve part 1 of the problem.
-    
-    Args:
-        data: Input data (list of lines or raw string)
-    
-    Returns:
-        Solution to part 1
-    """
-    # TODO: Implement part 1
     pass
 
 
 def part2(data):
-    """
-    Solve part 2 of the problem.
-    
-    Args:
-        data: Input data (list of lines or raw string)
-    
-    Returns:
-        Solution to part 2
-    """
-    # TODO: Implement part 2
     pass
 
 
 def main():
-    # Read input
-    # For line-by-line input:
-    # data = read_input(day=1)
-    
-    # For raw text input:
-    # data = read_input_raw(day=1)
-    
-    # Example with test input:
-    # data = read_input(day=1, filename="test_input.txt")
-    
-    data = read_input(day=1)
+    data = read_input(day=1, filename="input.txt")
     
     # Solve
     result1 = part1(data)
